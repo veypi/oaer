608e372dec1cd220efcc5fce9212986ef864fa03
veypi <i@veypi.com>
2023-10-05 18:39:50 +0800

    init oaer

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+ * index.ts
+ * Copyright (C) 2023 veypi <i@veypi.com>
+ * 2023-10-05 18:13
+ * Distributed under terms of the MIT license.
+ */
+
+import './index.css'
+import 'animate.css'
+import '@veypi/msg/index.css'
+import './assets/icon.js'
+
+import OAer from './components/main.vue'
+
+import { Cfg, api } from './api'
+
+export { OAer, Cfg, api }
