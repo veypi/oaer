c0941f03f23b3f038a1f0094bbbfa73c3985ccc5
veypi <i@veypi.com>
2023-10-05 21:26:41 +0800

    update

--- src/api/setting.ts
+++ src/api/setting.ts
@@ -9,20 +9,21 @@
 import { ref } from 'vue'
 
 export let Cfg = {
-  token: ref(''),
-  uuid: ref(''),
-  host: ref(''),
-  prefix: '/api',
-  BaseUrl() {
-    return this.host.value + this.prefix
-  },
-  goto(url: string) {
-    if (!url.startsWith('/')) {
-      url = '/' + url
-    }
-    window.location.href = this.host.value + '/#' + url
-  },
-  userFileUrl() {
-    return (this.host.value || window.location.host) + '/file/'
-  },
+    token: ref(''),
+    uuid: ref(''),
+    host: ref(''),
+    prefix: '/api',
+    BaseUrl() {
+        console.log(Cfg)
+        return this.host.value + this.prefix
+    },
+    goto(url: string) {
+        if (!url.startsWith('/')) {
+            url = '/' + url
+        }
+        window.location.href = this.host.value + '/#' + url
+    },
+    userFileUrl() {
+        return (this.host.value || window.location.host) + '/file/'
+    },
 }
