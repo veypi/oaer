f25000bdb6369a29eacc48d8beac9cc917e10924
veypi <i@veypi.com>
2023-10-17 03:58:42 +0800

    0.0.7

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { set_cfg } from './index.ts'
+import oaer from './index.ts'
 
-set_cfg({
+oaer.set({
   host: 'http://127.0.0.1:4001/',
   uuid: '816c7591414a450e80d848749cf89b17',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvYSIsImF1ZCI6IiIsImV4cCI6MTY5NzY5MTcxNSwiaWF0IjoxNjk3MzQ2MTE1LCJpZCI6ImVhM2U5YjQ3NTFiMjQwMWU4MGQ0MzY5Nzc4Yjk3YzE4Iiwibmlja25hbWUiOiJ2ZXlwaSIsImljb24iOiIvbWVkaWEvdXNlcl9pY29uL2VhM2U5YjQ3NTFiMjQwMWU4MGQ0MzY5Nzc4Yjk3YzE4LjE2OTcxMjMxMDgzODFNREV4T1M1cWNHYz0uanBnIiwiYWNjZXNzIjpbXX0.q2r2eom3w-UMmcloP5cr0UUeczIrM36ViMR3a_00aUo'
